9e9a7751ebda1574796cd51e50d710e69aea9ad0
Denyme24 <namanraj24@outlook.com>
2024-06-08 22:37:25 +0530

    Change CSS for the events and event specific page

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -8,6 +8,7 @@ const replace = (temp, product) => {
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
   output = output.replace(/{%ID%}/g, product.id || 'NO ID');
+  output = output.replace(/{%HOST_NAME%}/g, product.hostName);
   
   // if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
   return output;
